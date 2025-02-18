2584303fcdacdcbb3edc9d41d8df4ebb14756e5d
kihun0422 <kihun0422@naver.com>
2025-02-18 23:20:12 +0900

    test

---
+++ src/components/SplashScreen.js
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../styles/SplashScreen.css'; 
+import logo from '../assets/festimatelogo.png';
+
+const SplashScreen = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigate('/login');
+    }, 1500);
+  }, [navigate]);
+
+  return (
+    <div className="splash-container">
+      <div className="content">
+        <p className="title">축제에서 이상형 찾기!<br/>내 취향에 맞는 매칭 서비스!</p>
+        <img src={logo} alt="FestiMate Logo" className="logo" />
+      </div>
+    </div>
+  );
+};
+
+export default SplashScreen;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './styles/App.css';
+import './styles/SplashScreen.css'; // 스타일 적용
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
