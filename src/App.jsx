236cffc5dcd2e5d172f1bb6a1aef8be1e667c57f
kihun0422 <kihun0422@naver.com>
2025-03-16 01:11:43 +0900

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import React , { useEffect }from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import BeforeLogin from './components/Landing/Landing';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <div className='.container'>
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
-    </div>
-  )
+/*휴대폰 접속시 상단바 길이 반영 , pc접속시 북마크 등 기타 길이 반영 */
+function setScreenSize() {
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
 }
 
-export default App
-  
+const App = () => {
+  useEffect(() => { 
+    setScreenSize();
+    window.addEventListener("resize", setScreenSize);
+    return () => window.removeEventListener("resize", setScreenSize);
+  });
+
+  return (
+    <div className="page-container">
+        <Router>
+            <div className="content">
+                <Routes>
+                    <Route path="/" element={<BeforeLogin />} /> 
+                </Routes>
+            </div>
+        </Router>
+    </div>
+  );
+};
+
+export default App;
