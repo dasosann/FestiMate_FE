482f1b2b63e2529b2683c653b03681da3908e56b
dasosann <goodtony0403@gmail.com>
2025-03-15 23:22:48 +0900

    모바일 사이즈 세팅

--- src/pages/Main/MainPage.jsx
+++ src/pages/Main/MainPage.jsx
@@ -3,8 +3,8 @@ import M from '../../styles/pages/Main/MainPageStyle';
 const MainPage = () => {
     return (
         <M.HeaderDiv>
-            <img src="/images/Main/festimate-logo.svg" alt="로고" />
-            <img src="/images/Main/mainpage-menu.svg" alt="메뉴" />
+            <img src="/assets/Main/festimate-logo.svg" alt="로고" />
+            <img src="/assets/Main/mainpage-menu.svg" alt="메뉴" />
         </M.HeaderDiv>
     );
 };
