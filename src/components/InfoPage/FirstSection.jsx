f85d3f3073a4966385f97234b05ffe9c3dd7eab0
kihun0422 <kihun0422@naver.com>
2025-05-04 13:35:44 +0900

    input type tel

--- src/components/InfoPage/FirstSection.jsx
+++ src/components/InfoPage/FirstSection.jsx
@@ -123,6 +123,7 @@ const FirstSection = ({setCurrentPage, name, setName, phone, setPhone}) => {
                         <input
                         type="tel"
                         inputmode="numeric"
+                         pattern="[0-9]*"
                         className={`info-input ${phone.length > 0 ? 'hasContent' : ''}`}
                         placeholder="ex) 010-1234-5678"
                         value={phone}
