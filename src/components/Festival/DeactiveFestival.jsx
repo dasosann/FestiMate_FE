5da4903871eac3e9fa6cfc59a8338def6c87a504
kihun0422 <kihun0422@naver.com>
2025-05-05 14:10:21 +0900

    fix: 종료된 페스티벌 페이지

--- src/components/Festival/DeactiveFestival.jsx
+++ src/components/Festival/DeactiveFestival.jsx
@@ -7,6 +7,7 @@ import point_ from '/assets/Festival/point.svg';
 import noMatch from '/assets/Festival/no-match.svg';
 import endFestival from '/assets/Festival/end-festival.svg';
 import rightArrow from '/assets/Festival/arrow-right.svg';
+import info from '/assets/Festival/info-btn.svg';
 import instance from '../../../axiosConfig';
 
 import newProfile from '/assets/Profile/new-type-profile.svg';
@@ -57,7 +58,7 @@ const DeactiveFestival = ({festivalName, festivalDate, festivalId}) => {
                             <div className="festival-name-box">
                                 <span className="point-color">{festivalName}</span>
                                 <br />
-                                참여중이에요!
+                                종료된 페스티벌이에요!
                             </div>
                             <div className="festival-time-box">
                                 <img src={date} alt="날짜" />
@@ -75,9 +76,13 @@ const DeactiveFestival = ({festivalName, festivalDate, festivalId}) => {
                     </div>
                 </div>
                 <div className="end-notice">
-                    종료된 페스티벌로 더 이상의 서비스 이용이 불가합니다.<br/>
-                    더 자세한 환불 문의는 festimate2025@gmail.com
+                    <div className="info-icon-container">
+                        <img src={info} />
+                    </div>
+                    <span> 종료된 페스티벌로 더 이상의 서비스 이용이 불가합니다.<br/>
+                    더 자세한 환불 문의: festimate2025@gmail.com</span>
                 </div>
+            
                 <div className="festival-bottom-container">
                     <div className="festival-matching-container-wrapper">
                         <div className="festival-matching-content">
@@ -87,7 +92,7 @@ const DeactiveFestival = ({festivalName, festivalDate, festivalId}) => {
                 </div>
                 <button
                         className='refund-button'
-                        onClick={() => navigate('/mypage')}
+                        onClick={() => window.open('https://www.notion.so/1bcaebccb8e480f4ad57fdf7adb53ead')}
                 >
                     포인트 환불 신청하기
                 </button>
