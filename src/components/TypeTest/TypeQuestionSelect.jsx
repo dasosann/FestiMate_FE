de855f0bea394e457065d51ff10d13032bdec4fb
dasosan <goodtony12@naver.com>
2025-05-09 00:58:30 +0900

    type test 헤더 원상복구

--- src/components/TypeTest/TypeQuestionSelect.jsx
+++ src/components/TypeTest/TypeQuestionSelect.jsx
@@ -80,7 +80,7 @@ const TypeQuestionSelect = ({ setStarted, setCompleted, setFestivalType, festiva
         setCompleted(true);
       } catch (error) {
         console.error('데이터 전송 실패:', error);
-        alert('답변 제출 중 오류가 발생했습니다. 다시 시도해주세요.');
+        navigate('/mainpage')
       } finally {
         setIsLoading(false); // 로딩 종료
       }
--- src/styles/pages/TypeTest/TypeResultStyle.js
+++ src/styles/pages/TypeTest/TypeResultStyle.js
@@ -29,7 +29,6 @@ R.HeaderDiv = styled.div`
     margin-bottom: 40px;
     padding: 0 16px;
     margin-top: 12px;
-    position: fixed;
 `
 R.Left = styled.div`
     flex: 1;
